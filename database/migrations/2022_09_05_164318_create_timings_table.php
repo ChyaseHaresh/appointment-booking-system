9082dc9928c3d652b4c30bfa10dbde9d613027da
ChyaseHaresh <thisisharesh@gmail.com>
2022-09-05 22:42:45 +0545

    times migration added

---
+++ database/migrations/2022_09_05_164318_create_timings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timings', function (Blueprint $table) {
+            $table->id();
+            $table->time('avlbl_time');
+            $table->date('avlbl_date');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timings');
+    }
+};
